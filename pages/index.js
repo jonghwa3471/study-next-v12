9413c0efcf8ca3624efc840dd853bf5ab928059a
Jonghwa <whdghk3471@gmail.com>
2025-03-08 17:01:26 +0900

    #7.1 Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
-import Head from "next/head";
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "0e7caaa451bd63724c4d4ff302137c3e";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
